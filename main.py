9d80648abd17f3980f1db0f59f64f67f040a1c2b
mtosatti33 <56703672+mtosatti33@users.noreply.github.com>
2022-07-22 10:28:37 -0300

    commit #2

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     for i in team_dict.keys():
         print("---------------------------------------------")
         print("scrapping {}".format(team_dict[i]["Name"]))
-        Downloader.pull_rosters(None, team_dict[i])
+        Downloader.pull_rosters(team_dict[i])
         Downloader.pull_transactions(team_dict[i])
 
     Condenser.condense_all_data()
